efc927e24d4484cbe133424ff91a7b88ae59d526
Lakshitha Elapatha <lakshitha.e@gmail.com>
2021-06-04 23:46:18 +0530

    impliment endpoints

--- index.js
+++ index.js
@@ -3,8 +3,20 @@ const express = require('express');
 const PORT = 8080;
 const app = express();
 
-app.get('/', (req, res) => {
+app.get('/', function (req, res) {
   res.send([]);
+});
+
+app.post('/todo', function (req, res) {
+  res.send('success');
+});
+
+app.patch('/todo/:id', function (req, res) {
+  res.send('success');
+});
+
+app.delete('/todo/:id', function (req, res) {
+  res.send('success');
 });
 
 app.listen(PORT, () => console.log(`App listening on port ${PORT}!`));
