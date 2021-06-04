1bdbdaf507bb5ecb691f1faf78540f364b8fe170
Lakshitha Elapatha <lakshitha.e@gmail.com>
2021-06-04 23:53:21 +0530

    impliment todo service

--- index.js
+++ index.js
@@ -3,20 +3,10 @@ const express = require('express');
 const PORT = 8080;
 const app = express();
 
-app.get('/', function (req, res) {
-  res.send([]);
+router.get('/', function (req, res) {
+  res.send('Welcome to Todo App');
 });
 
-app.post('/todo', function (req, res) {
-  res.send('success');
-});
-
-app.patch('/todo/:id', function (req, res) {
-  res.send('success');
-});
-
-app.delete('/todo/:id', function (req, res) {
-  res.send('success');
-});
+app.use('/todo',require('./services/todo'))
 
 app.listen(PORT, () => console.log(`App listening on port ${PORT}!`));
