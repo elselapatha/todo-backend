c64984fa9bb3ddcd8e2ecf194260520c69df5dc6
Lakshitha Elapatha <lakshitha.e@gmail.com>
2021-06-04 23:54:07 +0530

    fix variable issue

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const PORT = 8080;
 const app = express();
 
-router.get('/', function (req, res) {
+app.get('/', function (req, res) {
   res.send('Welcome to Todo App');
 });
 
